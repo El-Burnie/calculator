fa3ffa905acb233f9e1de5aef6592e7bb1c42cdc
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-07-09 17:00:10 -0400

    Add basic arithmetic functions

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function add(a = 0, b = 0) {
+    return a + b;
+}
+
+function subtract(a = 0, b = 0) {
+    return a - b;
+}
+
+function multiply(a = 0, b = 0) {
+    return a * b;
+}
+
+function divide(a = 0, b = 1) {
+     return a / b;
+}
