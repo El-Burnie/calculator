d500e8e6b995f6c8734c92bfbc98144991b7f1d0
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-07-10 16:26:37 -0400

    Add global variables
    Used the Object.freeze() function on the OPERATIONS object to implement the object as an enum. This is because I do not want the list of operations to be modified, only read from.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
+let numberFirst = 0;
+let numberSecond = 0;
+let operator;
+const OPERATIONS = Object.freeze({
+    ADD: "ADD",
+    SUBTRACT: "SUBTRACT",
+    MULTIPLY: "MULTIPLY",
+    DIVIDE: "DIVIDE"
+});
+
 function add(a = 0, b = 0) {
     return a + b;
 }
